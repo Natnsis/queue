b09d47507acf01e000df3f738862bde39e1582e3
Nati <nsisay49@gmail.com>
2025-12-23 15:11:28 +0300

    all set up

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from "react-native";
+export default function Index() {
+
+  return (
+    <View>
+      <Text>
+        Hello with Plus Jakarta Sans!
+      </Text>
+    </View>
+  );
+}
