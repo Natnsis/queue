66fe05d7125474043375886d3ec7585a590a6897
Nati <nsisay49@gmail.com>
2026-01-06 15:55:28 +0300

    logo with header

--- lib/protected/dashboard.dart
+++ lib/protected/dashboard.dart
@@ -1,22 +1,91 @@
-import "package:flutter/material.dart";
-import "package:queue/widgets/header.dart";
-import "package:queue/widgets/scan.dart";
+import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(10),
+    final w = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(14),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Header(),
-              SizedBox(height: 20),
-              Scan(),
-              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      fit: BoxFit.cover,
+                      height: 35,
+                      width: 35,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    "Queue",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              Container(
+                height: h * 0.25,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade100,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(children: [
+
+                ],),
+              ),
+
+              const SizedBox(height: 20),
+
+              Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
             ],
           ),
         ),
