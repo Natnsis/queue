227d2595aa15b92d9d3f1c13274e1a08485f3292
Nati <nsisay49@gmail.com>
2026-01-08 11:30:27 +0300

    auth sha

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.queue"
+    namespace = "com.myapp.queue"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.queue"
+        applicationId = "com.myapp.queue"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
 import "package:flutter/material.dart";
 import "package:queue/onboarding.dart";
+import "package:queue/protected/tab.holder.dart";
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(QueueApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://lwsfmgkvpkcrjhhxjhdu.supabase.co',
+    anonKey: 'sb_publishable_E-8ysutEL7rdr9bAIvciaA_x3Ai84pA',
+  );
+  final session = Supabase.instance.client.auth.currentSession;
+
+  runApp(QueueApp(isLoggedIn: session != null));
 }
 
 class QueueApp extends StatelessWidget {
-  const QueueApp({super.key});
+  final bool isLoggedIn;
+  const QueueApp({super.key, required this.isLoggedIn});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Onboarding());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: isLoggedIn ? const Tabs() : const Onboarding(),
+    );
   }
 }
