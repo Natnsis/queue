fa1e7a2ce3dabc266acfa041be8396616e7dc570
Nati <nsisay49@gmail.com>
2026-02-12 15:32:57 +0300

    stack conversion

--- lib/widgets/active.queue.dart
+++
@@ -1,9 +0,0 @@
-import "package:flutter/material.dart";
-
-class ActiveQueue extends StatelessWidget {
-  const ActiveQueue({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text("Queues"));
-  }
-}
--- lib/widgets/past.queue.dart
+++
@@ -1,9 +0,0 @@
-import "package:flutter/material.dart";
-
-class PastQueue extends StatelessWidget {
-  const PastQueue({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text("Queues"));
-  }
-}
