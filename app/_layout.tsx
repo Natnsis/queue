c5fe9bc7800c8cad7ef4421e4b5de1af5da01c58
Nati <nsisay49@gmail.com>
2026-02-13 00:11:35 +0300

    fonts selection

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,14 +6,27 @@ import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import { useFonts } from 'expo-font'
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
+
+  const [loaded] = useFonts({
+    regular: require("@/assets/fonts/regular.ttf"),
+    bold: require("@/assets/fonts/bold.ttf"),
+    boldItalic: require("@/assets/fonts/bold-italic.ttf"),
+    italic: require("@/assets/fonts/italic.ttf"),
+    medium: require("@/assets/fonts/medium.ttf"),
+    mediumItalic: require("@/assets/fonts/medium-italic.ttf"),
+    semiBold: require("@/assets/fonts/semi-bold.ttf"),
+    semiBoldItalic: require("@/assets/fonts/semi-bold-italic.ttf"),
+  });
+
+  if (!loaded) return null;
 
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
