13cf549bc45c01adaeb36cfaf27e9da8086146f1
Nati <nsisay49@gmail.com>
2026-02-13 12:49:51 +0300

    onboarding image

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native';
+import { colors } from '@/assets/contants';
+import { View, Text, Image, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const index = () => {
+  const { height } = Dimensions.get('screen');
   return (
-    <View>
-      <Text>hehe</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        height: '100%'
+      }}
+    >
+      <View style={{
+        height: height * 0.6,
+        backgroundColor: colors.foreground
+      }}
+        className='p-2 rounded-b-[30px]'
+      >
+        <Image
+          source={require('@/assets/images/crowd.png')}
+          style={{
+            width: "100%",
+            height: '100%'
+          }}
+        />
+      </View>
+      <View style={{
+      }}>
+        <Text
+          style={{
+            fontFamily: 'regular',
+            fontSize: 30,
+            color: colors.text
+          }}
+        >
+          Transfer, track, and take charge within seconds
+        </Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
---
+++ assets/contants.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  foreground: "#F0F0F0",
+  text: "#1A1A1A",
+  ash: "#999999",
+  primary: "#F1BF00"
+}
