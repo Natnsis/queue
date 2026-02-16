8fda7066129174a697c74691a04f462f927732ff
Nati <nsisay49@gmail.com>
2026-02-16 21:41:50 +0300

    header and camera placeholder

--- app/tabs/scan/index.tsx
+++ app/tabs/scan/index.tsx
@@ -1,6 +1,9 @@
-import { View } from "react-native"
-import { Text } from "@/components/ui/text"
+import { View, Text } from "react-native"
+import ScanHeader from "@/components/ScanHeader"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { colors } from "@/assets/contants"
+import { Feather } from '@expo/vector-icons';
+import { Button } from "@/components/ui/button";
 
 const index = () => {
   return (
@@ -10,7 +13,71 @@ const index = () => {
       }}
       className="p-3"
     >
-      <Text>hehe</Text>
+      <ScanHeader />
+      <View
+        style={{
+          height: '35%',
+        }}
+        className="mt-4 px-6"
+      >
+        <View
+          style={{
+            backgroundColor: colors.foreground,
+            height: '100%',
+          }}
+          className="justify-center items-center rounded-lg"
+        >
+          <Feather name="maximize" size={90} color={colors.primary} />
+        </View>
+
+        <View className="px-6 mt-5">
+          <View
+            className="flex-row justify-between">
+            <Button
+              style={{
+                backgroundColor: colors.primary
+              }}
+              className="w-2/3"
+            >
+              <Text
+                style={{
+                  fontFamily: 'regular'
+                }}
+              >
+                Scan now
+              </Text>
+            </Button>
+
+            <Button
+              style={{
+                backgroundColor: colors.ash
+              }}
+            >
+              <Feather name="image" size={20} />
+            </Button>
+          </View>
+        </View>
+
+        <View
+          style={{
+            backgroundColor: colors.foreground
+          }}
+          className="mt-5 p-2 rounded-lg">
+          <Text
+            style={{
+              fontFamily: "regular"
+            }}
+          >
+            Queue History
+          </Text>
+
+
+
+          <View>
+
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
---
+++ components/ScanHeader.tsx
@@ -0,0 +1,27 @@
+import { View, Text } from "react-native"
+
+const ScanHeader = () => {
+  return (
+    <View className="mt-3">
+      <Text
+        style={{
+          fontFamily: 'bold',
+          fontSize: 24,
+          lineHeight: 26
+        }}
+      >
+        Scan QR code
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'italic',
+          fontSize: 12,
+        }}
+      >
+        position your camera inside the frame
+      </Text>
+    </View>
+  )
+}
+
+export default ScanHeader
