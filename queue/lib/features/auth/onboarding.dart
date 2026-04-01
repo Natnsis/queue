67f686b895942ec88013bb8763e7771b7437ecc7
natnael <nsisay49@gmail.com>
2026-04-01 10:57:05 +0300

    feat: image on onboarding

---
+++ queue/lib/features/auth/onboarding.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[60],
+      body: Column(
+        children: [
+          Text(
+            'Coffeee shop',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.brown,
+              fontWeight: FontWeight(300),
+            ),
+          ),
+          Image(image: AssetImage('assets/landing/coffee.jpg')),
+          SizedBox(height: 20),
+          SizedBox(
+            width: 400,
+            height: 50,
+            child: FilledButton(
+              onPressed: () {},
+              child: Text(
+                'Get Started',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight(200)),
+              ),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.brown),
+                foregroundColor: WidgetStatePropertyAll(Colors.white12),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
