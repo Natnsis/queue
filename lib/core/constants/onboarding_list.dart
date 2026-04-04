89abb42ef9451dbbf97f692c4714316a18fbb00e
natnael <nsisay49@gmail.com>
2026-04-04 21:41:30 +0300

    queue onboarding example

--- lib/core/constants/onboarding_list.dart
+++ lib/core/constants/onboarding_list.dart
@@ -12,25 +12,19 @@ class OnboardingData {
 
 const List<OnboardingData> onboardingList = [
   OnboardingData(
-    image: "assets/home.jpg",
-    title: "Skip the Wait, Not the Fun.",
-    description:
-        "Join queues remotely and track your progress in real-time. Your time is too valuable for physical lines.",
-  ),
-  OnboardingData(
-    image: "assets/queue.jpg",
+    image: "assets/onboarding/onb2.png",
     title: "Scan and Step Away.",
     description:
         "Simply scan a QR code at the entrance or search for your favorite spots nearby to grab a spot in line instantly.",
   ),
   OnboardingData(
-    image: "assets/notification.jpg",
+    image: "assets/onboarding/onb3.png",
     title: "Live Updates, Zero Stress.",
     description:
-        "Get notified when it’s almost your turn. We’ll ping you when you’re 3rd in line so you can head back.",
+        "Get notified when it's almost your turn. We'll ping you when you're 3rd in line so you can head back.",
   ),
   OnboardingData(
-    image: "assets/notification.jpg",
+    image: "assets/onboarding/onb4.png",
     title: "You’re Up Next!",
     description:
         "Show your digital ticket at the counter and enjoy your service. No paper, no shouting, no hassle.",
--- lib/features/auth/onboarding.dart
+++ lib/features/auth/onboarding.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:queue/core/constants/onboarding_list.dart';
 
-class Onboarding extends StatelessWidget {
-  const Onboarding({super.key});
+class onBoardingScreen extends StatefulWidget {
+  const onBoardingScreen({super.key});
 
   @override
+  State<onBoardingScreen> createState() => _onBoardingScreenState();
+}
+
+class _onBoardingScreenState extends State<onBoardingScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('whell hello boi')));
+    return Scaffold(
+      body: SafeArea(
+        child: PageView.builder(
+          itemCount: onboardingList.length,
+          itemBuilder: (context, index) {
+            final item = onboardingList[index];
+            return Column(
+              children: [
+                Image.asset(item.image),
+                Text(item.title),
+                Text(item.description),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
